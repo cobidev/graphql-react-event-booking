b41f4f2d6b5a462c59a4d7241317b060b5b84de0
Jacobo Martinez <jacobojavier98@gmail.com>
2019-08-25 21:56:15 -0400

    Setup initial graphQL API

--- app.js
+++ app.js
@@ -1,10 +1,40 @@
 const express = require('express');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 
 express.json();
 
-app.get('/', (req, res, next) => res.send('Hello World!'));
+app.use(
+  '/graphql',
+  graphqlHttp({
+    schema: buildSchema(`
+      type RootQuery {
+        events: [String!]!
+      }
+
+      type RootMutation {
+        createEvent(name: String): String
+      }
+
+      schema {
+        query: RootQuery
+        mutation: RootMutation
+      }
+    `),
+    rootValue: {
+      events: () => {
+        return ['Romantinc Cooking', 'Sailing', 'All-Night Coding'];
+      },
+      createEvent: args => {
+        const eventName = args.name;
+        return eventName;
+      }
+    },
+    graphiql: true
+  })
+);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server started at port: ${PORT}`));
