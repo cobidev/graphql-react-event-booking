f8b510cc1a1b16384369029ae5a194a52aff3beb
Jacobo Martinez <jacobojavier98@gmail.com>
2019-08-27 00:05:18 -0400

    Create Event type schema; create Event input; setup resolvers to create Events based from the eventInput

--- app.js
+++ app.js
@@ -4,18 +4,34 @@ const { buildSchema } = require('graphql');
 
 const app = express();
 
+const events = [];
+
 express.json();
 
 app.use(
   '/graphql',
   graphqlHttp({
     schema: buildSchema(`
+      type Event {
+        _id: ID!
+        title: String!
+        description: String!
+        price: Float!
+        date: String!
+      }
+
+      input EventInput {
+        title: String!
+        description: String!
+        price: Float!
+      }
+
       type RootQuery {
-        events: [String!]!
+        events: [Event!]!
       }
 
       type RootMutation {
-        createEvent(name: String): String
+        createEvent(input: EventInput): Event
       }
 
       schema {
@@ -25,11 +41,18 @@ app.use(
     `),
     rootValue: {
       events: () => {
-        return ['Romantinc Cooking', 'Sailing', 'All-Night Coding'];
+        return events;
       },
       createEvent: args => {
-        const eventName = args.name;
-        return eventName;
+        const event = {
+          _id: Math.random().toString(),
+          title: args.input.title,
+          description: args.input.description,
+          price: +args.input.price,
+          date: new Date().toISOString()
+        };
+        events.push(event);
+        return event;
       }
     },
     graphiql: true
